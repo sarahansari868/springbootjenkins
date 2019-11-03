5ae88481b62e808cf4ac1e731bf43b6ef7896043
sarahansari <sarah.ansari868>
2019-11-03 16:33:07 -0500

    Add simple time printing controller

---
+++ src/main/java/springapplication/TimeNow.java
@@ -0,0 +1,18 @@
+package springapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@RestController
+public class TimeNow {
+    @RequestMapping
+    public String Index() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return "The time now is: " + now.toString();
+    }
+}
