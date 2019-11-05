5ea0b6047b04f7ae26ba5e7e68177c05db1f862c
sarahansari <sarah.ansari868>
2019-11-05 10:23:26 -0500

    hello 3 controller

---
+++ src/main/java/springapplication/Test.java
@@ -0,0 +1,13 @@
+package springapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @RequestMapping("/Test")
+    public String index()
+
+    {
+        return ("Hi from Jwnkins");
+    }}
